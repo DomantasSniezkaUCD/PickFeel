f8fea67cbb1aa035bc3775dd5e3886243f648dda
Domantas Sniezka <domantas.sniezka@ucdconnect.ie>
2020-04-17 13:57:07 +0100

    Integrated system usability form to capture user feedback

--- app_pickfeel/models.py
+++ app_pickfeel/models.py
@@ -3,14 +3,11 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-
-class Category1(models.Model):
-    title = models.CharField(max_length=30)
-
-
-class Category2(models.Model):
-    title = models.CharField(max_length=30)
-
-
-class Category3(models.Model):
-    title = models.CharField(max_length=30)
+# # User Model
+# class User(models.Model):
+#
+#
+# # Image Model
+#
+#
+# # Data Model
