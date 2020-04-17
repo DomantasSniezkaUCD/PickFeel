b0ba6b215842bb2ea147201a6ba1075336fa0849
Domantas Sniezka <domantas.sniezka@ucdconnect.ie>
2020-04-17 12:55:40 +0100

    fixed minor bug

--- app_info/views.py
+++ app_info/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def pickfeelinfo(request):
     return render(request, 'app_info/info.html')
 
